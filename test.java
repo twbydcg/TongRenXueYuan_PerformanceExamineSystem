13dee9263a6b03ca396f30e39d04e6387ccbd289
maoshengquan <1585748457@qq.com>
2018-09-29 12:43:26 +0800

    delete test.java

--- test.java
+++
@@ -1,5 +0,0 @@
-public static void main(String[] args) {
-		//Connection connection1=DBConnection.getConnection();
-		//Connection connection2=DBConnection.getConnection();
-		//System.out.println(connection==connection2);  
-	}
