9c8313f85970c1569c298747c9e84beb6627b8eb
maoshengquan <39396594+maoshengquan@users.noreply.github.com>
2018-09-29 12:34:24 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+		//Connection connection1=DBConnection.getConnection();
+		//Connection connection2=DBConnection.getConnection();
+		//System.out.println(connection==connection2);  
+	}
