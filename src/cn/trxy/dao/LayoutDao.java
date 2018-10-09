28fd14e811d09da02c76a7a78293e30210b05cec
maoshengquan <1585748457@qq.com>
2018-10-09 22:59:49 +0800

    欧空空旷

--- src/cn/trxy/dao/LayoutDao.java
+++ src/cn/trxy/dao/LayoutDao.java
@@ -1,24 +1,61 @@
 package cn.trxy.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.trxy.bean.SignunitBean;
 import cn.trxy.inter.BaseInter;
+import cn.trxy.utils.DBConnection;
 
 /**
  * 版面
  * */
 public class LayoutDao implements BaseInter {
 
-	@Override
-	public <T> List<T> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	//数据库连接对象
+			private Connection connection=null;
+			//结果集
+			private ResultSet resultSet=null;
+			//连接状态
+			private PreparedStatement statement;
 
-	@Override
-	public Object getById() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> List<T> getAll() {
+				connection=DBConnection.getConnection();
+				List<SignunitBean> list=new ArrayList<SignunitBean>();
+				//组织SQL语句
+				String sql="select * from trxypes_layout";
+				try {
+					statement=connection.prepareStatement(sql);
+					resultSet=statement.executeQuery();
+					if(resultSet!=null) {
+						while(resultSet.next()) {
+							SignunitBean tem=new SignunitBean();
+							tem.setId(resultSet.getInt(1));
+							tem.setName(resultSet.getString(2));
+							list.add(tem);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}finally {
+					DBConnection.close(statement, resultSet);
+				}
+				if(list.size()>0) {
+					return (List<T>) list;
+				}else {
+					return null;
+				}
+			}
+
+			@Override
+			public Object getById() {
+				return null;
+			}
+
 
 }
